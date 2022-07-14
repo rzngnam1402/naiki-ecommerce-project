0010cea20ee22f3f7dea8de8ff5b5a568644f8c0
rzngnam1402 <legiangnam1402@gmail.com>
2022-07-14 23:43:04 +0700

    implementing brands api

---
+++ pages/api/brands/index.js
@@ -0,0 +1,30 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+export default async function getAllBrands(req, res) {
+    const allBrands = await prisma.shoes.findMany({
+        select: {
+            Brand: true
+        }
+    })
+
+    const seen = new Set();
+
+    const uniqBrands = allBrands.filter(el => {
+        const duplicates = seen.has(el.Brand);
+        seen.add(el.Brand);
+        return !duplicates;
+    })
+
+    const filteredBrands = [];
+    uniqBrands.map(el => {
+        filteredBrands.push(el.Brand);
+    })
+
+    try {
+        res.json(filteredBrands);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- pages/brands/index.js
+++ pages/brands/index.js
@@ -1,19 +1,46 @@
 import React from 'react'
+import { getAllBrands } from '../../lib/api-utils'
 import classes from './brands.module.css'
+import useSWR from 'swr'
 
-const dummy_brands = [
-    {
-        name: "Adidas",
-        price: "10",
-            
-    }
-]
-const Brands = () => {
+const Brands = (props) => {
+
+    const { data, error } = useSWR('api/brands', (url) => fetch(url).then(res => res.json()));
+    if (error) return <div>An error occured.</div>
+    if (!data) return <div>Loading ...</div>
+
+    console.log(data[1].Brand);
+
     return (
         <>
-
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <p>lmao</p>
+            <ul>
+                {data.map(brand => {
+                    <li key={brand.Brand}>{`${brand.Brand}`}</li>
+                })}
+            </ul>
         </>
     )
 }
+
+// export async function getServerSideProps() {
+//     const allBrands = await getAllBrands();
+
+//     return {
+//         props: {
+//             allBrands: allBrands,
+//         },
+//     }
+// }
 
 export default Brands
