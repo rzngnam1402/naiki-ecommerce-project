b06b2eb297df32d0f315e94aa336cbb7b1162f86
rzngnam1402 <legiangnam1402@gmail.com>
2022-07-18 16:35:35 +0700

    updating products card and products images components

--- components/products-details/products-card.js
+++ components/products-details/products-card.js
@@ -1,14 +1,73 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from './products-card.module.css'
+import Aos from 'aos'
+import "aos/dist/aos.css"
+
+const getSize = (sizeString) => {
+    const firstSizeRange = 0;
+    const lastSizeRange = 0;
+
+    if (sizeString.length > 2) {
+        firstSizeRange = Number(sizeString[0]);
+        if (sizeString[4])
+            lastSizeRange = Number(sizeString[3] + sizeString[4]);
+        else
+            lastSizeRange = Number(sizeString[3])
+
+        return {
+            firstSizeRange: firstSizeRange,
+            lastSizeRange: lastSizeRange
+        }
+    }
+    else
+        return -1;
+}
 
 const ProductsCard = (props) => {
     const { data } = props;
 
     const detail = data[0];
 
+    const size = getSize(detail.Size);
+    let sizeRange = new Array(size.lastSizeRange - size.firstSizeRange || 0)
+    if (size !== -1) {
+        let num = size.firstSizeRange;
+        for (let j = 0; j <= size.lastSizeRange - size.firstSizeRange; j++) {
+            sizeRange[j] = num;
+            num++;
+        }
+    }
+
+    useEffect(() => {
+        Aos.init({ duration: 500 });
+    }, [500])
+
     return (
-        <div className={classes.container}>
-            <h2>{detail.Brand}</h2>
+        <div data-aos="fade-up" className={classes.container}>
+            <h2 className={classes.brand}>{detail.Brand}</h2>
+            <h2 className={classes.name}>{detail.Name}</h2>
+            <h3 className={classes.type}>{detail.Gender} {detail.Type}</h3>
+            <p className={classes.price}>{`$${detail.Price}.00`}</p>
+
+            {size === -1 ? (
+                <>
+                    <p className={classes.size}>Selected size: </p>
+                    <select className={classes.select}>
+                        <option value="0">{detail.Size}</option>
+                    </select>
+                </>
+            ) : (
+                <>
+                    <p className={classes.size}>Selected size: </p>
+                    <select className={classes.select}>
+                        {sizeRange.map((size, index) => (
+                            <option value={index}>{size}</option>
+                        ))}
+                    </select>
+                </>
+            )}
+            <br />
+            <button className={classes.button}>Add to cart</button>
         </div>
     )
 }
--- components/products-details/products-images.js
+++ components/products-details/products-images.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import classes from './products-images.module.css'
+import Aos from 'aos'
+import "aos/dist/aos.css"
 
 const ProductsImages = (props) => {
+    useEffect(() => {
+        Aos.init({ duration: 500 });
+    }, [500])
+
     const { id } = props;
 
     return (
         <>
             <div className={classes.container}>
-                <Image className={classes.img} src={`/images/ImageDetails/${id}/0.jpg`} width={1200} height={1200} />
-                <Image className={classes.img} src={`/images/ImageDetails/${id}/1.jpg`} width={1200} height={1200} />
-                <Image className={classes.img} src={`/images/ImageDetails/${id}/2.jpg`} width={1200} height={1200} />
-                <Image className={classes.img} src={`/images/ImageDetails/${id}/3.jpg`} width={1200} height={1200} />
+                <Image data-aos="fade-right" className={classes.img} src={`/images/ImageDetails/${id}/0.jpg`} width={1200} height={1200} />
+                <Image data-aos="fade-right" className={classes.img} src={`/images/ImageDetails/${id}/1.jpg`} width={1200} height={1200} />
+                <Image data-aos="fade-right" className={classes.img} src={`/images/ImageDetails/${id}/2.jpg`} width={1200} height={1200} />
+                <Image data-aos="fade-right" className={classes.img} src={`/images/ImageDetails/${id}/3.jpg`} width={1200} height={1200} />
             </div>
         </>
     )
