92762262cbcc9030c28cb49148c55e65e76c5e02
rzngnam1402 <legiangnam1402@gmail.com>
2022-07-20 16:58:39 +0700

    adding Link to featured sneaker components

--- components/featured-sneaker/featured-sneaker.js
+++ components/featured-sneaker/featured-sneaker.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import Link from 'next/dist/client/link'
 import classes from './featured-sneaker.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
@@ -17,23 +18,30 @@ const FeaturedSneaker = () => {
                     <p>Bringing a new look the Nike Waffle One blances everything you love most about
                         heritage Nike running with fresh innovations.
                     </p>
-                    <button>Buy Now <FontAwesomeIcon icon={faArrowRight} /></button>
+                    <Link href="./brands/Nike">
+                        <button>Buy Now <FontAwesomeIcon icon={faArrowRight} /></button>
+                    </Link>
                 </div>
-
-                <div className={classes.image}>
-                    <img src="images/Featured/2.jpg" alt="" />
-                </div>
+                <Link href="./brands/Nike">
+                    <div className={classes.image}>
+                        <img src="images/Featured/2.jpg" alt="" />
+                    </div>
+                </Link>
             </div>
 
             <div data-aos="fade-right" className={classes.container}>
-                <div className={classes.image}>
-                    <img src="images/Featured/1.jpg" alt="" />
-                </div>
+                <Link href='./brands/Adidas'>
+                    <div className={classes.image}>
+                        <img src="images/Featured/1.jpg" alt="" />
+                    </div>
+                </Link>
                 <div className={classes.description}>
                     <h1>Adidas Originals</h1>
                     <p>Discover the adidas streetwear collection
                     </p>
-                    <button>Buy Now <FontAwesomeIcon icon={faArrowRight} /></button>
+                    <Link href='./brands/Adidas'>
+                        <button>Buy Now <FontAwesomeIcon icon={faArrowRight} /></button>
+                    </Link>
                 </div>
             </div>
         </>
