31d2ed69b97e7a6848fd6f6926b2f1532046cd93
rzngnam1402 <legiangnam1402@gmail.com>
2022-08-16 17:00:04 +0700

    add contact form validation

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -6,8 +6,6 @@ const dev = process.env.NODE_ENV !== 'production';
 export const server = dev ? 'http://localhost:3000' : 'https://naiki-ecommerce-project.vercel.app';
 
 const ContactForm = () => {
-
-    console.log(server);
     const nameInputRef = useRef();
     const phoneInputRef = useRef();
     const emailInputRef = useRef();
@@ -15,7 +13,6 @@ const ContactForm = () => {
 
     function submitFormHandler(event) {
         event.preventDefault();
-
         const enteredName = nameInputRef.current.value;
         const enteredPhone = phoneInputRef.current.value;
         const enteredEmail = emailInputRef.current.value;
@@ -54,21 +51,26 @@ const ContactForm = () => {
                         <h1>Please leave us some comments !</h1>
                     </div>
                     <div className={classes.content}>
-                        <input
+                        <input className={classes.name_input}
                             type="text"
                             placeholder="Your Name"
+                            pattern="^[A-Za-z0-9]{3,16}$"
                             required
+                            onChange={onChange}
                             ref={nameInputRef}
                         />
-                        <input
+                        <input className={classes.email_input}
                             type="email"
                             placeholder="Email Address"
+                            pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
                             required
                             ref={emailInputRef}
                         />
-                        <input
-                            type="number"
+                        <input className={classes.phone_input}
+                            type="text"
                             placeholder="Phone Number"
+                            pattern="(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b"
+                            required
                             ref={phoneInputRef}
                         />
                         <input type="text" placeholder="Address" />
@@ -77,6 +79,9 @@ const ContactForm = () => {
                             ref={messageInputRef}
                         ></textarea>
                         <button onClick={submitFormHandler}>Submit</button>
+                        <span className={classes.name_span}>Your name should be 3-16 characters long and should not have special characters</span>
+                        <span className={classes.email_span}>You should enter a valid email</span>
+                        <span className={classes.phone_span}>You should enter a valid Vietnamese phone number ex:0966244758</span>
                     </div>
                 </div>
             </form>
