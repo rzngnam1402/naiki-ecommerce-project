32ed5c3d53939c1a7a7688312c855e1e8b0bed7d
rzngnam1402 <legiangnam1402@gmail.com>
2022-12-22 15:39:41 +0700

    update sql files

--- naikiii.sql
+++ naikiii.sql
@@ -74,8 +74,7 @@ INSERT INTO Shoes(Name,Brand,Size,Color,Type,Gender,Price,Image) VALUES
 ('ACG AIR MADA SNEAKER','Nike','5, 15','dark-milky-white','sneaker','unisex','145.00','/images/Nike/9999205855441_1_20'),
 ('Air Trainer 1 SP','Nike','10','black','sneaker','unisex','150.00','/images/Nike/9999205853676_1_18'),
 ('AIR MAX TERRASCAPE 90 NI','Nike','6, 8','white','sneaker','female','150.00','/images/Nike/9999207928068_1_4'),
-('ACG Mountain Fly Low GORE-TEX
-','Nike','5, 9','white','sneaker','unisex','180.00','/images/Nike/9999205852839_1'),
+('ACG Mountain Fly Low GORE-TEX','Nike','5, 9','white','sneaker','unisex','180.00','/images/Nike/9999205852839_1'),
 ('W AF1 Pixel','Nike','6, 9','black','sneaker','female','120.00','/images/Nike/9999206671392_1'),
 ('BLAZER LOW 77 JUMBO','Nike','8','white','sneaker','unisex','95.00','/images/Nike/9999207769432_1_3'),
 ('NIKE AIR HUARACHE','Nike','6, 8','white','sneaker','female','120.00','/images/Nike/9999203540820_1'),
@@ -100,4 +99,64 @@ INSERT INTO Shoes(Name,Brand,Size,Color,Type,Gender,Price,Image) VALUES
 ('W AIR FORCE 1 07 LOW NH NN','Nike','6, 9','white','sneaker','female','130.00','/images/Nike/9999203544163_1_10'),
 ('Dunk Low Se','Nike','6, 8','white','sneaker','female','110.00','/images/Nike/9999202317560_1_10'),
 ('Air Max 95 Se','Nike','9, 10','blue','sneaker','unisex','180.00','/images/Nike/9999202297190_1_9'),
-('Nike Free Run Trail','Nike','7','grey','sneaker','male','120.00','/images/Nike/9999205426016_1_29')
+('Nike Free Run Trail','Nike','7','grey','sneaker','male','120.00','/images/Nike/9999205426016_1_29');
+
+ UPDATE shoes SET stripeid = 'price_1LhSfjHbWieXXAGk4hrGmzgS' WHERE shoesid = 53;
+ UPDATE shoes SET stripeid = 'price_1LhSf0HbWieXXAGkkfXf9eXn' WHERE shoesid = 52;
+ UPDATE shoes SET stripeid = 'price_1LhSeIHbWieXXAGkMvDs0UIP' WHERE shoesid = 51;
+ UPDATE shoes SET stripeid = 'price_1LhSceHbWieXXAGk5GC0lQZa' WHERE shoesid = 50;
+ UPDATE shoes SET stripeid = 'price_1LhSbiHbWieXXAGkpDATNu0H' WHERE shoesid = 49;
+ UPDATE shoes SET stripeid = 'price_1LhSa7HbWieXXAGkeNyrOk3m' WHERE shoesid = 48;
+ UPDATE shoes SET stripeid = 'price_1LhPZcHbWieXXAGk5AfmY2pS' WHERE shoesid = 47;
+ UPDATE shoes SET stripeid = 'price_1LhSZCHbWieXXAGkXK5Ijpo9' WHERE shoesid = 46;
+ UPDATE shoes SET stripeid = 'price_1LhSYKHbWieXXAGkfLQ8RSDK' WHERE shoesid = 45;
+ UPDATE shoes SET stripeid = 'price_1LhSXcHbWieXXAGkxPLdjgee' WHERE shoesid = 44;
+ UPDATE shoes SET stripeid = 'price_1LhSWxHbWieXXAGkusd23Amm' WHERE shoesid = 43;
+ UPDATE shoes SET stripeid = 'price_1LhSWEHbWieXXAGkEyyc7wun' WHERE shoesid = 42;
+ UPDATE shoes SET stripeid = 'price_1LhSVfHbWieXXAGkW1PsILvv' WHERE shoesid = 41;
+ UPDATE shoes SET stripeid = 'price_1LhSUzHbWieXXAGk1AumK4O5' WHERE shoesid = 40;
+ UPDATE shoes SET stripeid = 'price_1LhSUEHbWieXXAGkhWPGuCIs' WHERE shoesid = 39;
+ UPDATE shoes SET stripeid = 'price_1LhSTdHbWieXXAGkS2WsoXap' WHERE shoesid = 38;
+ UPDATE shoes SET stripeid = 'price_1LhSSrHbWieXXAGkYh2GaaOD' WHERE shoesid = 37;
+ UPDATE shoes SET stripeid = 'price_1LhSSCHbWieXXAGk4Pm7yjp7' WHERE shoesid = 36;
+ UPDATE shoes SET stripeid = 'price_1LhSRUHbWieXXAGkg3FDPSuf' WHERE shoesid = 35;
+ UPDATE shoes SET stripeid = 'price_1LhSQmHbWieXXAGkSGIn6JBx' WHERE shoesid = 34;
+ UPDATE shoes SET stripeid = 'price_1LhSPyHbWieXXAGkEnZ7Gz2e' WHERE shoesid = 33;
+ UPDATE shoes SET stripeid = 'price_1LhSPGHbWieXXAGkZg0VHwXo' WHERE shoesid = 32;
+ UPDATE shoes SET stripeid = 'price_1LhSNdHbWieXXAGk3qIliaby' WHERE shoesid = 31;
+ UPDATE shoes SET stripeid = 'price_1LhSMzHbWieXXAGkztsJhgAe' WHERE shoesid = 30;
+ UPDATE shoes SET stripeid = 'price_1LhSM8HbWieXXAGk72f06diV' WHERE shoesid = 29;
+ UPDATE shoes SET stripeid = 'price_1LhSL9HbWieXXAGkiIFpDYuI' WHERE shoesid = 28;
+ UPDATE shoes SET stripeid = 'price_1LhSKAHbWieXXAGkXOiL4gIu' WHERE shoesid = 27;
+ UPDATE shoes SET stripeid = 'price_1LhSJKHbWieXXAGkuyGexpBW' WHERE shoesid = 26;
+ UPDATE shoes SET stripeid = 'price_1LhSIaHbWieXXAGk59WE11QC' WHERE shoesid = 25;
+ UPDATE shoes SET stripeid = 'price_1LhSHfHbWieXXAGkhbNTGnwh' WHERE shoesid = 24;
+ UPDATE shoes SET stripeid = 'price_1LhSGCHbWieXXAGkbF9uiFpE' WHERE shoesid = 23;
+ UPDATE shoes SET stripeid = 'price_1LhSFQHbWieXXAGk6XpW6oOl' WHERE shoesid = 22;
+ UPDATE shoes SET stripeid = 'price_1LhSDhHbWieXXAGkG8N6fJkV' WHERE shoesid = 21;
+ UPDATE shoes SET stripeid = 'price_1LhSCiHbWieXXAGkocwmAqME' WHERE shoesid = 20;
+ UPDATE shoes SET stripeid = 'price_1LhS9BHbWieXXAGkuRC6Lmtj' WHERE shoesid = 19;
+ UPDATE shoes SET stripeid = 'price_1LhS8RHbWieXXAGkGQbqVB28' WHERE shoesid = 18;
+ UPDATE shoes SET stripeid = 'price_1LhS7cHbWieXXAGkBWOD8MhK' WHERE shoesid = 17;
+ UPDATE shoes SET stripeid = 'price_1LhS6UHbWieXXAGkvA7EDXRP' WHERE shoesid = 16;
+ UPDATE shoes SET stripeid = 'price_1LhS5nHbWieXXAGkdfp36JjM' WHERE shoesid = 15;
+ UPDATE shoes SET stripeid = 'price_1LhS4hHbWieXXAGkI59SMRfT' WHERE shoesid = 14;
+ UPDATE shoes SET stripeid = 'price_1LhS44HbWieXXAGkDqRdgLxX' WHERE shoesid = 13;
+ UPDATE shoes SET stripeid = 'price_1LhS3MHbWieXXAGkmHAnMHuW' WHERE shoesid = 12;
+ UPDATE shoes SET stripeid = 'price_1LhS2eHbWieXXAGkAoGdKewQ' WHERE shoesid = 11;
+ UPDATE shoes SET stripeid = 'price_1LhS1RHbWieXXAGkz4RfYFW4' WHERE shoesid = 10;
+ UPDATE shoes SET stripeid = 'price_1LhS0HHbWieXXAGk33bSS4m8' WHERE shoesid = 9;
+ UPDATE shoes SET stripeid = 'price_1LhRyCHbWieXXAGkyqGlgL43' WHERE shoesid = 8;
+ UPDATE shoes SET stripeid = 'price_1LhRxQHbWieXXAGk3et3uR1f' WHERE shoesid = 7;
+ UPDATE shoes SET stripeid = 'price_1LhRwbHbWieXXAGknPlTENSJ' WHERE shoesid = 6;
+ UPDATE shoes SET stripeid = 'price_1LhRvnHbWieXXAGkbfovU85Z' WHERE shoesid = 5;
+ UPDATE shoes SET stripeid = 'price_1LhRp8HbWieXXAGkI8JOtkXl' WHERE shoesid = 4;
+ UPDATE shoes SET stripeid = 'price_1LhRoVHbWieXXAGkjprqslPt' WHERE shoesid = 3;
+ UPDATE shoes SET stripeid = 'price_1LhRntHbWieXXAGkEgj7peqa' WHERE shoesid = 2;
+ UPDATE shoes SET stripeid = 'price_1LhRnDHbWieXXAGkCpP5WFOK' WHERE shoesid = 1;
+ 
+
+
+ 
+
+
